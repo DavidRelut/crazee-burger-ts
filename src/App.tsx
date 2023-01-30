36c3d9505c841d8ac833313459e4cac27e02c73a
DavidR <codingaccelerator@gmail.com>
2023-01-30 11:59:42 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent, useState } from "react";
+
+const App: FunctionComponent = () => {
+  // const [count, setCount] = useState(0);
+
+  return <div className="App">Salut !</div>;
+};
+
+export default App;

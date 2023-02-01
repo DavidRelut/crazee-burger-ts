926fe6659806e781dd3841e085ccf03117d5e9ef
DavidR <codingaccelerator@gmail.com>
2023-02-02 00:27:06 +0100

    ♻️ F01: Remove useless div

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import { FunctionComponent } from "react";
 import { LoginPage } from "./components/pages/login/LoginPage";
 
 const App: FunctionComponent = () => {
-  return (
-    <div className="App">
-      <LoginPage />
-    </div>
-  );
+  return <LoginPage />;
 };
 
 export default App;
--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -2,9 +2,5 @@ import { FunctionComponent } from "react";
 import { LoginForm } from "./LoginForm";
 
 export const LoginPage: FunctionComponent = () => {
-  return (
-    <div className="App">
-      <LoginForm />
-    </div>
-  );
+  return <LoginForm />;
 };

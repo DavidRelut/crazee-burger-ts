21129b06b6be9ea49adeabbd56cd684ec98a2350
DavidR <codingaccelerator@gmail.com>
2023-02-02 15:31:16 +0100

    ✨ F02: APP > Install routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import { FunctionComponent } from "react";
+import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "./components/pages/login/LoginPage";
+import { OrderPage } from "./components/pages/OrderPage";
+import { ErrorPage } from "./components/pages/ErrorPage";
 
 const App: FunctionComponent = () => {
-  return <LoginPage />;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/order/:username" element={<OrderPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;

9639d61bfbd7650c15b8c3884c3342d1612133c4
DavidR <codingaccelerator@gmail.com>
2023-02-11 15:31:47 +0100

    💄 F03: LOGIN > Add styling and background

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,5 +1,37 @@
+import Logo from "../../reusable-ui/Logo";
 import { LoginForm } from "./LoginForm";
+import styled from "styled-components";
 
 export const LoginPage = () => {
-  return <LoginForm />;
+  return (
+    <LoginPageStyled>
+      <Logo />
+      <LoginForm />
+    </LoginPageStyled>
+  );
 };
+
+const LoginPageStyled = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  ::before {
+    content: "";
+    background: url("/images/burger-and-fries-background.jpg")
+      rgba(0, 0, 0, 0.7);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 1;
+  }
+`;
